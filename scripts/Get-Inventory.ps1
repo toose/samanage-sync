b557d92685c8760e09ac9bb8f630de1c4cc1287d
Christopher DiTusa <cditusa@gmail.com>
2020-09-02 19:20:12 -0400

    Parameterize Get-Inventory.ps1

--- scripts/Get-Inventory.ps1
+++ scripts/Get-Inventory.ps1
@@ -1,9 +1,17 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Position=1)]
+    [string] $Path,
+    [Parameter(Position=2)]
+    [string] $Destination
+)
+
 $inventory = @()
-$config = Get-Content .\config.json | ConvertFrom-Json
+$config = Get-Content $Path | ConvertFrom-Json
 
 foreach($domain in $config.Domains) {
     $inventory += Get-ADComputer -Filter * -SearchBase $domain.SearchBase -Server $domain.Server `
         -Properties $config.Properties | Select-Object name, @{n="owner";e={$_.description}}
 }
 
-Out-File -FilePath "$PSScriptRoot\inventory.json" -InputObject (ConvertTo-Json $inventory)
+Out-File -FilePath $Destination -InputObject (ConvertTo-Json $inventory)
